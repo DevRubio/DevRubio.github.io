469404873b2d8b1cb622d18863b78dd4f8eb9667
Oscar Rubio <ofrubio0@gmail.com>
2023-08-21 12:18:34 -0500

    (ADD) Se crea el componente Card y se completa Page Projects

---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,47 @@
+import { IconDemo, IconGithub } from "../../assets/icons";
+
+const Card = ({ Animate, Title, Description, Repo, Deploy }) => {
+  return (
+    <div
+      className={`max-w-sm p-6 bg-gray-700 border border-gray-700 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-3 ${Animate}`}
+    >
+      <h5 className="mb-2 text-2xl text-center font-bold tracking-tight text-white dark:text-white">
+        {Title}
+      </h5>
+
+      <p className="mb-3 font-normal text-justify text-white dark:text-gray-400 text-sm h-[140px]">
+        {Description}
+      </p>
+      
+      <div className="flex justify-between">
+        {Deploy ? (
+          <a
+            href={Deploy}
+            className="w-32 h-10 inline-flex justify-around items-center text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-3 py-2 text-center mr-2 mb-2 hover:animate-wiggle"
+            target="_blank"
+          >
+            Demo
+            <IconDemo />
+          </a>
+        ) : (
+          ""
+        )}
+
+        {Repo ? (
+          <a
+            href={Repo}
+            className="w-32  h-10 inline-flex justify-around items-center text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-3 py-2 text-center mr-2 mb-2 hover:animate-wiggle"
+            target="_blank"
+          >
+            Repo
+            <IconGithub size="20px" />
+          </a>
+        ) : (
+          ""
+        )}        
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/Pages/Projects/index.jsx
+++ src/Pages/Projects/index.jsx
@@ -1,11 +1,86 @@
-import Construction from "../Construction"
-const Projects = () =>{
-    return(
-        <section>
-            <Construction
-                page={"Projects"}
-            />
-        </section>
-    )
-}
-export { Projects }
+import Card from "../../Components/Card";
+import LineGradient from "../../Components/LineGradient";
+const Projects = () => {
+    const proyect = [
+        {
+            Animate: "animate-fade-left",
+            Title: "Closing Template",
+            Description:"Advanced AI application to enhance incident management messages. Real-time suggestions improve clarity, professionalism, and adherence to internal standards. Customizable and adaptable, it ensures efficient, consistent, and clear communication for effective incident resolution.",
+            Deploy: "https://closing-template.vercel.app/",
+            Repo: "",  
+        },{
+            Animate:"animate-fade-left animate-delay-500",
+            Title:"Chat Bot Ai",
+            Description: "The Chat Bot AI utilizes natural language processing to engage in meaningful conversations. With its advanced algorithms, it can understand and respond to user inputs, making interactions more seamless and efficient.",
+            Deploy: "",
+            Repo: "https://github.com/DevRubio/chat-bot-ai.git"
+        },{
+            Animate:"animate-fade-left animate-delay-1000",
+            Title:"Google Translate Clone",
+            Description: "The Google Translate Clone employs OpenAI's API to replicate language translation functionalities. This integration enables users to input text in one language and receive accurate translations, showcasing the potential of AI-powered language tools.",
+            Deploy: "",
+            Repo: "https://github.com/DevRubio/google-translate-clone.git"
+        },{
+            Animate:"animate-fade-left animate-delay-[1500ms]",
+            Title:"App ToDos",
+            Description: "The App-ToDos simplifies task management. With its user-friendly interface and intuitive features, it empowers users to efficiently organize their tasks, and track progress, ultimately enhancing productivity and time management.",
+            Deploy: "https://devrubio.github.io/App-ToDos/",
+            Repo: "https://github.com/DevRubio/App-ToDos.git"
+        }
+    ]
+  return (
+    <section className="pt-40 pb-20">
+        {/*Heading */}
+        <div className="md:w-2/4 mx-auto text-center animate-fade-down mb-10">            
+            <div>
+                <p className="font-playfair font-semibold text-2xl mb-5">
+                    PROJECTS
+                </p>
+                <div className="flex justify-center mt-5">
+                    <LineGradient width="w-1/3"/>
+                </div>                
+            </div>
+        </div>
+        {/*Projects */}
+        <div className="flex justify-center">
+            <div className="sm:grid sm:grid-cols-3 gap-4">                
+                {proyect.map(item =>(
+                    <Card
+                        key={item.Title}
+                        Animate={item.Animate}
+                        Title={item.Title}
+                        Description={item.Description}
+                        Deploy={item.Deploy}
+                        Repo={item.Repo}
+                    />
+                ))}
+            {/* <Card
+                Animate="animate-fade-left"
+                Title = "Closing Template"
+                Description = ""
+                Repo
+                Deploy
+            /> 
+                <Card
+                Animate="animate-fade-left animate-delay-500"
+                Title = "Closing Template"
+                Description = ""
+                Repo
+                Deploy
+            />  
+            <Card
+                Animate="animate-fade-left animate-delay-1000"
+                Title = "Closing Template"
+                Description = ""
+                Repo
+                Deploy
+            /> */}    
+            </div>
+            
+        </div>
+                              
+              
+    </section>
+  );
+};
+export { Projects };
